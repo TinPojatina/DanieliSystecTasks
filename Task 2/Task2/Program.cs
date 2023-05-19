e50fd0b3fd85f9c29547a5e2442fd9395fd34c5e
Tin Pojatina <tin.p99@gmail.com>
2023-05-19 14:31:50 +0200

    Task 4 object list generation

--- Task 2/Task2/Program.cs
+++ Task 2/Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Task2;
+
+List<Box> boxes= new List<Box>();
+
+Random rand = new Random();
+for (int i = 0; i < 10; i++)
+{
+    int length = rand.Next(1, 11);
+    int width = rand.Next(1, 11);
+    int height = rand.Next(1, 11);
+
+    boxes.Add(new Box(length, width, height));
+}
+
+foreach(Box box in boxes)
+{
+    Console.WriteLine($"Length {box.Width} Height: {box.Height} Width: {box.Height}");
+}
