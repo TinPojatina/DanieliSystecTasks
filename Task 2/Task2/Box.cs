8bb869d78c9abb928b0bf9fda48daac8538e507d
Tin Pojatina <tin.p99@gmail.com>
2023-05-19 12:55:23 +0200

    Task 2
    Set box class with basic attributes

---
+++ Task 2/Task2/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+   public class Box
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Box(int length, int width, int height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+    }
+}
